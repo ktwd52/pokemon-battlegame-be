8ece7697bcfa7efdfa81d1177b81fb00bb8c9c16
ktwd52 <tibor.keglovics@gmail.com>
2024-08-07 14:43:37 +0200

    MongoDB and getLeaderBoard and createUserScore added

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,1 +1,61 @@
 import leaderboard from "../models/leaderboard.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ErrorResponse from "../utils/ErrorResponse.js";
+import mongoose from "mongoose";
+
+export const getLeaderBoard = asyncHandler(async (req, res) => {
+  const LeaderBoard = await leaderboard.find();
+  // .populate("username", "wins", "losses");
+  res.status(200).json(LeaderBoard);
+});
+
+export const createUserScore = asyncHandler(async (req, res) => {
+  const { username, wins, losses } = req.body;
+
+  if (!username || wins == null || losses == null) {
+    throw new ErrorResponse("Please provide all required fields", 400);
+  }
+
+  // Find the user in the leaderboard
+  let user = await leaderboard.findOne({ username });
+
+  if (!user) {
+    // If user doesn't exist, create a new record
+    user = await leaderboard.create({ username, wins, losses });
+    await user.save();
+    return res.status(201).json(user);
+  } else {
+    // If user exists, update the record
+    user.wins = wins;
+    user.losses = losses;
+    await user.save();
+    return res.status(200).json(user); // Updated status code to 200 for successful update
+  }
+});
+
+// Delete all leaderboard entry and set id counter back to 0 -> send DELETE request to Server with JSON body: { "delete_leaderboard": "yes" }
+export const deleteLeaderBoard = asyncHandler(async (req, res) => {
+  const { delete_leaderboard } = req.body;
+
+  if (delete_leaderboard !== "yes") {
+    return res
+      .status(400)
+      .json({ message: "Invalid request to delete leaderboard" });
+  }
+
+  const dellb = await leaderboard.deleteMany({}); // returns deletedCount: 1 when deleted 0 when empty.
+
+  const delic = await mongoose.connection
+    .collection("identitycounters")
+    .deleteMany({}); // Ensure you replace 'identitycounters' with the actual collection name if different.
+
+  if (dellb.deletedCount === 0 && delic.deletedCount === 0) {
+    throw new ErrorResponse("Leaderboard is already empty", 404);
+  }
+
+  res.status(200).json({
+    message: "Leaderboard deleted successfully",
+    deletedLeaderboardCount: dellb.deletedCount,
+    deletedIdentityCountersCount: delic.deletedCount,
+  });
+});
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
+import {
+  getLeaderBoard,
+  createUserScore,
+  deleteLeaderBoard,
+} from "../controllers/leaderboard.js";
 
 const leaderboardRouter = Router();
 
-leaderboardRouter.route("/").get().post();
+leaderboardRouter
+  .route("/")
+  .get(getLeaderBoard)
+  .post(createUserScore)
+  .delete(deleteLeaderBoard);
 
 export default leaderboardRouter;
