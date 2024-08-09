34a77620b50d7e7dc146269cacc96c28eba06e42
hexzoner <hexzon@gmail.com>
2024-08-09 16:01:04 +0200

    getting userscore from api

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,16 +1,10 @@
 import { Router } from "express";
-import {
-  getLeaderBoard,
-  createUserScore,
-  deleteLeaderBoard,
-} from "../controllers/leaderboard.js";
+import { getLeaderBoard, createUserScore, deleteLeaderBoard, getUserScore } from "../controllers/leaderboard.js";
 
 const leaderboardRouter = Router();
 
-leaderboardRouter
-  .route("/")
-  .get(getLeaderBoard)
-  .post(createUserScore)
-  .delete(deleteLeaderBoard);
+leaderboardRouter.route("/").get(getLeaderBoard).post(createUserScore).delete(deleteLeaderBoard);
+
+leaderboardRouter.route("/:username").get(getUserScore);
 
 export default leaderboardRouter;
